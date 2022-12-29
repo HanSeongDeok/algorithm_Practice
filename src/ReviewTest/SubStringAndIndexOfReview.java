d42b32884ace25627f85c8a57e1acc8a86035382
HanSeongDeok <hsd970730@naver.com>
2022-12-30 00:41:28 +0900

    알고리즘 01-01~04 Review

---
+++ src/ReviewTest/SubStringAndIndexOfReview.java
@@ -0,0 +1,26 @@
+package ReviewTest;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class SubStringAndIndexOf {
+    public static void solution(String str){
+        String strArr[] = str.split(" ");
+        String answer = "";
+        int max = 0;
+        for (String s: strArr) {
+            if(s.length()>max) {
+                max = s.length();
+                answer = s;
+            } else continue;
+        }
+        System.out.println(answer);
+    }
+    public static void main(String[] args) throws IOException {
+        BufferedReader rb = new BufferedReader(new InputStreamReader(System.in));
+        String str = rb.readLine();
+        solution(str);
+    }
+}
