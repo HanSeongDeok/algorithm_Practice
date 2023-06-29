46443b22704b7082dacf6822a15c7dfd42543113
sdhan <hsd970730@naver.com>
2023-06-29 23:04:25 +0900

    Array And Hashing day5 - remind

---
+++ src/neetCode150/ArrayHashing/Longest_Consecutive_Sequence.java
@@ -0,0 +1,16 @@
+package neetCode150.ArrayHashing;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class Longest_Consecutive_Sequence {
+    public int longestConsecutive(int[] nums) {
+        if (nums.length == 0)
+            return 0;
+
+    }
+}
--- src/neetCode150/ArrayHashing/Valid_Sudoku.java
+++ src/neetCode150/ArrayHashing/Valid_Sudoku.java
@@ -1,29 +1,31 @@
 package neetCode150.ArrayHashing;
 
-import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
+
 
 public class Valid_Sudoku {
     Set<Character> rowSet = new HashSet<>();
     Set<Character> columnSet = new HashSet<>();
     boolean validRow = true;
     public boolean isValidSudoku(char[][] board) {
+        final int rowLen = board[0].length;
+        final int colLen = board.length;
+        final int boxLen = 9;
+
         // Check for row
-        boolean isValidRow = IntStream.range(0, board[0].length)
+        boolean isValidRow = IntStream.range(0, rowLen)
                 .allMatch(i -> {
                     Set<Character> rowSet = new HashSet<>();
-                    return IntStream.range(0, board.length)
+                    return IntStream.range(0, colLen)
                             .filter(j -> board[i][j] != '.') // Filter out empty cells
                             .mapToObj(j -> board[i][j])
                             .allMatch(rowSet::add);
                 });
 
         // Check for column
-        boolean isValidColumn = IntStream.range(0, board.length)
+        boolean isValidColumn = IntStream.range(0, rowLen)
                 .allMatch(i -> {
                     Set<Character> columnSet = new HashSet<>();
                     return IntStream.range(0, board[0].length)
@@ -33,24 +35,25 @@ public class Valid_Sudoku {
                 });
 
         // Check for block
-       /* boolean isValidBlock = IntStream.range(0, row)
-                .allMatch(i -> {
+        boolean isValidBlock = IntStream.range(0, boxLen)
+                .allMatch(num -> {
                     Set<Character> blockSet = new HashSet<>();
-                    int tempI = (i / 3) * 3;
-                    int tempJ = (i % 3) * 3;
-                    return IntStream.range(0, column)
-                            .filter(j -> j % 3 == 0 || j == 0)
-                            .allMatch(j -> {
-                                if (board[tempJ][tempI] == '.') {
-                                    tempJ++;
-                                    return true;
-                                }
-                                boolean isValid = blockSet.add(board[tempJ][tempI]);
-                                tempJ++;
-                                return isValid;
+                    final int tempI = (num / 3) * 3;
+                    final int tempJ = (num % 3) * 3;
+                    return IntStream.range(0, num).allMatch(index -> {
+                                char ch = board[tempJ + index % 3][tempI + index / 3];
+                                return ch == '.' || blockSet.add(ch);
                             });
-                });*/
+                });
 
-        return validRow;
+        return isValidRow & isValidColumn & isValidBlock;
+    }
+
+    public boolean isValidSudoku2(char[][] board) {
+
+
+
+        return false;
     }
 }
+
