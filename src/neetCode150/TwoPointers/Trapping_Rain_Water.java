726584ed08a564d613f3bcb04e3cba1c3bfb77ca
sdhan <hsd970730@naver.com>
2023-07-27 21:04:12 +0900

    Window Sliding remind day4 restart

--- src/neetCode150/TwoPointers/Trapping_Rain_Water.java
+++ src/neetCode150/TwoPointers/Trapping_Rain_Water.java
@@ -30,7 +30,7 @@ public class Trapping_Rain_Water {
     }
 
     public static int trap3(int[] height) {
-       int left = 0, right = height.length-1, water = 0, leftMax = 0, rightMax = 0;
+       int left = 0, right = height.length-1, water = 0, leftMax = height[left], rightMax = height[right];
        while (left < right) {
            leftMax = Math.max(leftMax, height[left]);
            rightMax = Math.max(rightMax, height[right]);
@@ -39,13 +39,24 @@ public class Trapping_Rain_Water {
         return water;
     }
 
-    //TODO STACK 구조체를 이용하여 풀이도 해보기
     public static int trap4(int[] height) {
+        int startIndex = 0, endIndex = height.length-1, winMax = 0, startMax = 0, endMax = 0;
+        while (startIndex < endIndex) {
+            startMax = Math.max(startMax, height[startIndex]);
+            endMax = Math.max(endMax, height[endIndex]);
+            winMax += (startMax <= endMax) ? startMax-height[startIndex++] : endMax -height[endIndex--];
+        }
+        return winMax;
+    }
+
+    //TODO STACK 구조체를 이용하여 풀이도 해보기
+    public static int trap5(int[] height) {
         return 0;
     }
 
     public static void main(String[] args) {
         System.out.println(trap2(new int[]{0,1,0,2,1,0,1,1,2,1,2,1}));
         System.out.println(trap3(new int[]{0,1,0,2,1,0,1,1,2,1,2,1}));
+        System.out.println(trap4(new int[]{0,1,0,2,1,0,1,1,2,1,2,1}));
     }
 }
