017d6efc496dd0b2768fe1321bdc658d0aef11e5
sdhan <hsd970730@naver.com>
2023-08-16 22:04:10 +0900

    stack 4ay

--- src/neetCode150/stack/Daily_Temperature.java
+++ src/neetCode150/stack/Daily_Temperature.java
@@ -16,7 +16,19 @@ public class Daily_Temperature {
         return temperatures;
     }
 
+    public static int[] dailyTemperatures2(int[] temperatures) {
+        Stack<Integer> stack = new Stack<>();
+        int[] temp = new int[temperatures.length];
+        for (int i=0; i < temperatures.length; i++) {
+            while (stack.size() > 0 && temperatures[i] > temperatures[stack.peek()]){
+                temp[stack.peek()] = i - stack.pop();
+            }
+            stack.push(i);
+        }
+        return temp;
+    }
+
     public static void main(String[] args) {
-        System.out.println(Arrays.toString(dailyTemperatures(new int[]{73,74,75,71,69,72,76,73})));
+        System.out.println(Arrays.toString(dailyTemperatures2(new int[]{73,74,75,71,69,72,76,73})));
     }
 }
