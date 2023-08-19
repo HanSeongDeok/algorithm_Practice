c0567e4831a0e3fb5997dbd88493d42b8f70ed42
sdhan <hsd970730@naver.com>
2023-08-19 21:35:06 +0900

    stack 6ay - end binary search 1day - start

---
+++ src/neetCode150/BinarySearch/Koko_Eating_Banana.java
@@ -0,0 +1,8 @@
+package neetCode150.BinarySearch;
+
+public class Koko_Eating_Banana {
+    public int minEatingSpeed(int[] piles, int h) {
+
+        return 0;
+    }
+}
---
+++ src/neetCode150/BinarySearch/Search_a_2D_Matrix.java
@@ -0,0 +1,14 @@
+package neetCode150.BinarySearch;
+
+public class Search_a_2D_Matrix {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i=0;
+        int j = matrix[0].length-1;
+        while (i < matrix.length && j >= 0) {
+            if (matrix[i][j] == target) return true;
+            else if(matrix[i][j] > target) j--;
+            else if(matrix[i][j] < target) i++;
+        }
+        return false;
+    }
+}
