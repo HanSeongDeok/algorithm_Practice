8fb0d4306b882d223d56e71a37411764f7168ed2
sdhan <hsd970730@naver.com>
2023-10-17 23:13:18 +0900

    LinkedList - 7day

--- src/neetCode150/LinkedList/LRUCache.java
+++ src/neetCode150/LinkedList/LRUCache.java
@@ -44,6 +44,40 @@ public class LRUCache {
         test.put(2,2);
         test.get(1);
         test.put(3,3);
+
+        int[] temp = {1,2,3,4,5,6};
+        NodeTmp first = new NodeTmp(1, temp[0]);
+        NodeTmp two = new NodeTmp(2, temp[1]);
+        first.next = two;
+        two.prev = first;
+        for (int i=2; i < temp.length; i++) {
+            NodeTmp tmp = new NodeTmp(i+1, temp[i]);
+            two.next = tmp;
+            tmp.prev = two;
+            two = two.next;
+        }
+        while (first.next != null){
+            System.out.println(first.toString());
+            first = first.next;
+        }
+        System.out.println(first.toString());
+
+        NodeTmp last = new NodeTmp(6, temp[5]);
+        NodeTmp lastHead = new NodeTmp(5, temp[4]);
+        lastHead.next = last;
+        last.prev = lastHead;
+        NodeTmp tempHead = lastHead;
+        for (int i=temp.length-3; i >= 0; i--){
+            NodeTmp tem = new NodeTmp(i+1, temp[i]);
+            tem.next = tempHead;
+            tem.next.prev = tem;
+            tempHead = tem;
+        }
+        while (tempHead.next != null) {
+            System.out.println(tempHead.toString());
+            tempHead = tempHead.next;
+        }
+        System.out.println(tempHead.toString());
     }
 }
 
@@ -68,7 +102,7 @@ class LinkedNodeList{
         NodeTmp tmp = dummyHead.next ;
         dummyHead.next = node;
         node.next = tmp;
-        //node.prev = dummyHead;
+        node.prev = dummyHead;
         tmp.prev = node;
     }
 
@@ -92,5 +126,8 @@ class NodeTmp{
     NodeTmp(int key, int value){
         this.key = key;
         this.val =value;
+    }
+    public String toString(){
+        return String.valueOf(val);
     }
 }
