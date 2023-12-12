41c27e3967bb15fdf2ffaa384ec4075290b2c82d
sdhan <hsd970730@naver.com>
2023-12-12 22:40:25 +0900

    programmers start - Level3 Banned

---
+++ src/Programmers/Banned.java
@@ -0,0 +1,38 @@
+package Programmers;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class Banned {
+    public int solution(String[] user_id, String[] banned_id) {
+        Set<Set<String >> set = new HashSet<>();
+        set = bannedDfs(set, user_id, banned_id, 0, new HashSet<>());
+        return set.size();
+    }
+    private Set<Set<String>> bannedDfs(Set<Set<String>> setResult, String[] userIds, String[] bannedIds, int depth, Set<String> set) {
+        boolean check = true;
+        if (bannedIds.length == depth) {
+            setResult.add(set); return setResult;
+        }
+
+        for (int i=0; i < userIds.length; i++) {
+            if (set.contains(userIds[i]) || userIds[i].length() != bannedIds[depth].length()) continue;
+            for (int j = 0; j < userIds[i].length(); j++) {
+                if (bannedIds[depth].charAt(j) != '*' && bannedIds[depth].charAt(j) != userIds[i].charAt(j)) {
+                    check = false; break;
+                }
+                check = true;
+            }
+            if (check) {
+                set.add(userIds[i]);
+                setResult = bannedDfs(setResult, userIds, bannedIds, depth+1, new HashSet<>(set));
+                set.remove(userIds[i]);
+            }
+        }
+        return setResult;
+    }
+
+    public static void main(String[] args) {
+        new Banned().solution(new String[]{"frodo", "fradi", "crodo", "abc123", "frodoc"}, new String[]{"fr*d*", "*rodo", "******", "******"});
+    }
+}
